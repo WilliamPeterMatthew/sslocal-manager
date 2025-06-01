4d564604ff17a6608df2b19d74a109b6cd0f9679
WilliamPeterMatthew <zkx20020420@foxmail.com>
2025-06-01 11:47:29 +0800

    modified config

--- manager/app.py
+++ manager/app.py
@@ -1,8 +1,19 @@
 import os
 import subprocess
+import re
+import platform
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+kernel_ver = platform.release()
+match = re.search(r'(\d+)\.(\d+)', kernel_ver)
+cmv, csv = (int(match.group(1)), int(match.group(2))) if match else (0, 0)
+TFO = ""
+if cmv >= 3 and csv > 7:
+    tfo = "--fast-open"
+
+ARGS=os.getenv("ARGS","")
 
 def get_status():
     try:
@@ -40,7 +51,10 @@ def action():
         subprocess.Popen([
             "ss-local",
             "-c", "/.ssconfig.json",
-            "-f", "/var/run/ss-local.pid"
+            "-f", "/var/run/ss-local.pid",
+            "-u",
+            TFO,
+            ARGS,
         ])
     
     return redirect('/')
